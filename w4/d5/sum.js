1c2fea83f1fac452eb1a6ffe7cf984b93f67f870
Alvin Ng <alvinoalvin123@gmail.com>
2021-04-23 22:30:12 +0000

    big O example code

---
+++ w4/d5/sum.js
@@ -0,0 +1,20 @@
+function arrayContainsSum(array, sum) {
+  let i = 0; // 1
+  let ii = array.length - 1; // 1
+
+  while (i <= ii) { // n + 1
+
+    const element1 = array[i]; // n
+    const element2 = array[ii]; // n
+    const currentSum = element1 + element2; // n
+
+    if (currentSum === sum) { // n
+      return true;
+    } else if (currentSum > sum) { // n
+      ii--; // n
+    } else {
+      i++;
+    }
+  }
+  return false; // 1
+}
